53b0a3d7f14f12e97d94dc97abf8272f1643bb1b
max <920523327@qq.com>
2020-05-11 02:21:59 +0800

    新增我的页面

---
+++ pages/mime/mime.js
@@ -0,0 +1,14 @@
+var util = require('../../utils/util.js')
+var request = require('../../utils/https.js')
+var app = getApp()
+Page({
+    data: {
+        userInfo: {}
+    },
+    //事件处理函数
+    bindViewTap: function () {
+
+    },
+    onLoad: function () {
+    }
+})
