70fb61311c9e774f243de30e64d1c26fb6347dc2
big399 <y1244181098@163.com>
2020-05-11 00:51:00 +0800

    找回的detail.js文件

--- pages/product/gouwu/details.js
+++ pages/product/gouwu/details.js
@@ -1,33 +1,33 @@
 var http = require('../../../utils/https.js')
 
 Page({
-    /**
-     * 页面的初始数据
+    /** 
+     * 页面的初始数据 
      */
     data: {
         detail: null,
-        //是否显示指示点
+        //是否显示指示点 
         indicatordots: true,
-        //指示点颜色
-        indicatorcolor:"white"
+        //指示点颜色 
+        indicatorcolor: "white"
     },
 
-    /**
-     * 生命周期函数--监听页面加载
+    /** 
+     * 生命周期函数--监听页面加载 
      */
-    onLoad: function(options) {
+    onLoad: function (options) {
         /** 页面一加载就访问服务器接口，加载商品列表 */
         var that = this;
-        //上个页面传递的id
+        //上个页面传递的id 
         var data_id = options.data_id
         var url = '/product/' + data_id + '/withDetails'
 
-        http.getReq(url, null, function(res) {
-            //将获取到的数据，存在名字叫detail的这个对象中
+        http.getReq(url, null, function (res) {
+            //将获取到的数据，存在名字叫detail的这个对象中 
             that.setData({
-                    hidden: true,
-                    detail: res,
-                }),
+                hidden: true,
+                detail: res,
+            }),
                 console.log(res);
         })
     },
@@ -38,10 +38,10 @@ Page({
         console.log(postId);
     },
 
-    /**
-     * 用户点击右上角分享
+    /** 
+     * 用户点击右上角分享 
      */
-    onShareAppMessage: function() {
+    onShareAppMessage: function () {
 
     }
 })
