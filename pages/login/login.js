2b2b12553e97bcaa0dcc4583ae4792da94ee7332
max <920523327@qq.com>
2020-05-14 20:48:43 +0800

    更改图片文件夹路径

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,9 +1,10 @@
-// pages/login/login.js
-Page({
+// login.js
+var util = require('../../utils/util.js')
+var request = require('../../utils/https.js')
+var uri = '/manager/logOn'
+var app = getApp()
 
-    /**
-     * 页面的初始数据
-     */
+Page({
     data: {
 
     },
@@ -11,56 +12,65 @@ Page({
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function (options) {
+    onLoad: function(options) {
 
     },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
-    onReady: function () {
+    onReady: function() {
 
     },
 
     /**
      * 生命周期函数--监听页面显示
      */
-    onShow: function () {
+    onShow: function() {
 
     },
 
     /**
      * 生命周期函数--监听页面隐藏
      */
-    onHide: function () {
+    onHide: function() {
 
     },
 
     /**
      * 生命周期函数--监听页面卸载
      */
-    onUnload: function () {
+    onUnload: function() {
 
     },
 
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
-    onPullDownRefresh: function () {
+    onPullDownRefresh: function() {
 
     },
 
     /**
      * 页面上拉触底事件的处理函数
      */
-    onReachBottom: function () {
+    onReachBottom: function() {
 
     },
 
     /**
      * 用户点击右上角分享
      */
-    onShareAppMessage: function () {
+    onShareAppMessage: function() {
 
+    },
+
+    /**
+     * 微信用户授权
+     */
+    onLogin: function() {
+        app.getUserInfo(function(userInfo) {
+            console.log("userInfo: " + userInfo)
+        })
     }
 })
