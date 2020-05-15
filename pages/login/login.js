b395757f0e0c9983e6940b9474c14d325231b0d1
max <920523327@qq.com>
2020-05-15 21:44:26 +0800

    登录页

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,76 +1,26 @@
 // login.js
+import {
+    get,
+    post
+} from '../../utils/network.js'
 var util = require('../../utils/util.js')
-var request = require('../../utils/https.js')
 var uri = '/manager/logOn'
-var app = getApp()
+const app = getApp()
 
 Page({
-    data: {
-
+    onLoad() {
+        get('/page/foxIndex', {name: "hhh"}).then(res => {
+            console.log("测试GET请求")
+            console.log(res)
+        }).catch(err => {
+            console.log(err)
+        })
     },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function(options) {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function() {
-
-    },
-
-    /**
-     * 微信用户授权
-     */
-    onLogin: function() {
-        app.getUserInfo(function(userInfo) {
-            console.log("userInfo: " + userInfo)
+    handleGetUserInfo(event) {
+        console.log(event)
+        wx.showModal({
+            title: '用户信息',
+            content: event.detail.userInfo.nickName
         })
     }
 })
