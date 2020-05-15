91bbb50cc651f80ecfffc3d23eb6eb11e5762dc5
max <920523327@qq.com>
2020-05-15 20:07:29 +0800

    封装网络请求的修改

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
 //app.js
 App({
-    onLaunch: function() {
+    onLaunch() {
         // 展示本地存储能力
         var logs = wx.getStorageSync('logs') || []
         logs.unshift(Date.now())
         wx.setStorageSync('logs', logs)
-    },
-    getUserInfo: function(cb) {
-        var that = this;
-        if (this.globalData.userInfo) {
-            typeof cb == 'function' && cb(this.globalData)
-        } else {
-            // 调用微信登录接口
-            wx.login({
-                success: function(res) {
-                    console.log("code=" + res.code)
-                }
-            })
-        }
     },
     globalData: {
         userInfo: null
--- utils/https.js
+++ utils/https.js
@@ -1,44 +1,40 @@
 var rootDocment = 'http://app.imcfp.com/api/app'
-
-function getReq(url, data, callback) {
-    wx.showLoading({
-        title: '加载中...',
-    })
-    wx.request({
-        url: rootDocment + url,
-        'Content-Type': 'application/json',
-        data: data,
-        method: 'GET',
-        success: function(res) {
-            wx.hideLoading();
-            return typeof callback == 'function' && callback(res.data)
-        },
-        fail: function(e) {
-            wx.hideLoading();
-            wx.showModal({
-                title: '网络错误',
-                content: '网络出错，请刷新重试',
-                showCancel: false
-            })
-            return typeof callback == 'function' && callback(e)
-        }
-    })
+var header = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Authorization': "Bearer " + wx.getStorageSync("token"),
+    'os': 'android',
+    'version': '1.0.0'
 }
 
-function postReq(url, data, callback) {
+function get(url, params, callback) {
+    request(url, params, "GET", callback);
+}
+
+function post(url, params, callback) {
+    request(url, params, "POST", callback);
+}
+
+/**
+ * 封装网络请求
+ * @url URL地址
+ * @params 请求参数
+ * @method 请求方式：GET/POST
+ * @callback 回调函数
+ */
+function request(url, params, method, callback) {
     wx.showLoading({
         title: '加载中...',
     })
     wx.request({
         url: rootDocment + url,
-        'Content-Type': 'application/x-www-form-urlencoded',
-        data: data,
-        method: 'POST',
-        success: function(res) {
+        header: header,
+        method: method,
+        data: params,
+        success: (res) => {
             wx.hideLoading();
             return typeof callback == 'function' && callback(res.data)
         },
-        fail: function(e) {
+        fail: (e) => {
             wx.hideLoading();
             wx.showModal({
                 title: '网络错误',
@@ -51,6 +47,6 @@ function postReq(url, data, callback) {
 }
 
 module.exports = {
-    getReq: getReq,
-    postReq: postReq
+    getRequest: get,
+    postRequest: post
 }
